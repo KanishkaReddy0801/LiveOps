2315754b1223fa93c285b1c64b4129a510694d47
KanishkaReddy0801 <krishnakanishkareddyalla@gmail.com>
2023-02-25 17:25:37 +0530

    adding frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Offers from './pages/Offers';
+import OfferCreate from './pages/OfferCreate';
+import OfferEdit from './pages/OfferEdit';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Login/>} />
+        <Route exact path="/offers" element={<Offers />} />
+        <Route exact path="/offer/create" element={<OfferCreate/>} />
+        <Route exact path="/offers/:id/edit" component={<OfferEdit/>} />
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
